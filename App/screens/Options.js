52aac05b6ac865bc9a9dbfad49ffd889bf169066
Anthony Okpako <melowsworld@yahoo.co.uk>
2023-05-11 18:35:25 +0100

    setting up home screen

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,36 +1,48 @@
-import React from "react"
-import { SafeAreaView } from "react-native"
-import { Entypo } from "@expo/vector-icons"
+import React from "react";
+import { Alert, Linking, SafeAreaView, ScrollView } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 
-import colors from "../constants/colors"
-import { RowItem, RowSeparator } from "../components/RowItem"
+import colors from "../constants/colors";
+import { RowItem, RowSeparator } from "../components/RowItem";
+
+const openUrl = (url) => {
+  Linking.openURL(url).catch(() => {
+    Alert.alert("Sorry, something went wrong.", "Please try again.");
+  });
+};
 
 export default () => {
   return (
-    <SafeAreaView>
-      <RowItem
-        title="Themes"
-        onPress={() => alert("todo!")}
-        rightIcon={
-          <Entypo name="chevron-right" size={20} color={colors.blue} />
-        }
-      />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <RowItem
+          title="Themes"
+          onPress={() => alert("todo!")}
+          rightIcon={
+            <Entypo name="chevron-right" size={20} color={colors.blue} />
+          }
+        />
 
-      <RowSeparator />
+        <RowSeparator />
 
-      <RowItem
-        title="React Native Basics"
-        onPress={() => alert("todo!")}
-        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
-      />
+        <RowItem
+          title="React Native Basics"
+          onPress={() =>
+            openUrl(
+              "https://learn.handlebarlabs.com/p/react-native-basics-build-a-currency-converter"
+            )
+          }
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
 
-      <RowSeparator />
+        <RowSeparator />
 
-      <RowItem
-        title="React Native by Example"
-        onPress={() => alert("todo!")}
-        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
-      />
+        <RowItem
+          title="React Native by Example"
+          onPress={() => alert("todo!")}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
+      </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
