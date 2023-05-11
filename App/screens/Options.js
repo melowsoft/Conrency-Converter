da11e3681fbdda3a3b9b5c2a49c67251368c6eba
Anthony Okpako <melowsworld@yahoo.co.uk>
2023-05-11 09:33:33 +0100

    first commit

---
+++ App/screens/Options.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { SafeAreaView } from "react-native"
+import { Entypo } from "@expo/vector-icons"
+
+import colors from "../constants/colors"
+import { RowItem, RowSeparator } from "../components/RowItem"
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <RowItem
+        title="Themes"
+        onPress={() => alert("todo!")}
+        rightIcon={
+          <Entypo name="chevron-right" size={20} color={colors.blue} />
+        }
+      />
+
+      <RowSeparator />
+
+      <RowItem
+        title="React Native Basics"
+        onPress={() => alert("todo!")}
+        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+      />
+
+      <RowSeparator />
+
+      <RowItem
+        title="React Native by Example"
+        onPress={() => alert("todo!")}
+        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+      />
+    </SafeAreaView>
+  )
+}
