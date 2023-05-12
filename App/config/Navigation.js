25ce5f24a42454ee6b05e2390525bd7b85968620
Anthony Okpako <melowsworld@yahoo.co.uk>
2023-05-12 15:42:31 +0100

    Added the currency list screen

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Entypo } from "@expo/vector-icons";
 
 import Home from "../screens/Home";
 import Options from "../screens/Options";
+import CurrencyList from "../screens/CurrencyList";
+import colors from "../constants/colors";
 
 const MainStack = createStackNavigator();
 const MainStackScreen = () => (
@@ -17,8 +21,35 @@ const MainStackScreen = () => (
   </MainStack.Navigator>
 );
 
+const ModalStack = createStackNavigator();
+const ModalStackScreen = () => (
+  <ModalStack.Navigator mode="modal">
+    <ModalStack.Screen
+      name="main"
+      component={MainStackScreen}
+      options={{ headerShown: false }}
+    />
+    <ModalStack.Screen
+      name="CurrencyList"
+      component={CurrencyList}
+      options={({ route, navigation }) => ({
+        title: route.params && route.params.title,
+        headerLeft: null,
+        headerRight: () => (
+          <TouchableOpacity
+            style={{ paddingHorizontal: 10 }}
+            onPress={() => navigation.pop()}
+          >
+            <Entypo name="cross" size={30} color={colors.blue} />
+          </TouchableOpacity>
+        ),
+      })}
+    />
+  </ModalStack.Navigator>
+);
+
 export default () => (
   <NavigationContainer>
-    <MainStackScreen />
+    <ModalStackScreen />
   </NavigationContainer>
 );
